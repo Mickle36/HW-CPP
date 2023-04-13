8abed7bbe528b3f0bb52d2daa6e009cc7754e359
Mickle36 <maltsev0492@list.ru>
2023-04-13 19:46:57 +1100

    changed main_3.2

--- Lesson_3/task_3.2/main_3.2.cpp
+++ Lesson_3/task_3.2/main_3.2.cpp
@@ -9,97 +9,85 @@ protected:
 
 public:
 
-	Counter()
+	Counter(int schet, std::string val)
 	{
-		schetchik = 0;
-		value = "";
+		this->schetchik = schet;
+		this->value = val;
 	}
 
-	void set_schetchik()
+	void set_plus_schetchik()
 	{
-		int schet;
-
-		if (value == "yes")
-		{
-			std::cout << "Enter the initial value of the counter: ";
-			std::cin >> schet;
-			schetchik = schet;
-		}
-		else if (value == "no")
-		{
-			std::cout << "Counter value: " << schetchik << std::endl;
-		}
-		else
-		{
-			std::cout << "Error! repeated question..." << std::endl;
-		}
+		schetchik++;
 	}
 
-	void your_value(std::string val)
+	void set_minus_schetchik()
 	{
-		value = val;
+		schetchik--;
 	}
 
-	void effect(std::string val)
+	void set_equals_schetchik()
 	{
-		if (val == "+")
-		{
-			schetchik++;
-		}
-		else if (val == "-")
-		{
-			schetchik--;
-		}
-		else if (val == "=")
-		{
-			std::cout << "Counter value: " << schetchik << std::endl;
-		}
-		else if (val == "x")
-		{
-			std::cout << "Goodbuy!" << std::endl;
-		}
+		std::cout << "Counter value: " << schetchik << std::endl;
 	}
 
-	bool bool_val(std::string val)
-	{
-		if (val == "yes")
-		{
-			return true;
-		}
-		else if (val == "no")
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
 };
 
 int main()
 {
 	std::string val;
+	int schet;
 	bool bool_value = false;
 
 
-	Counter new_Counter;
-
 	do {
-		while (bool_value == false)
+	loop:
+		std::cout << "Do you want to specify the initial value of the counter ? Enter yes or no : ";
+		std::cin >> val;
+
+		if (val == "yes")
 		{
-			std::cout << "Do you want to specify the initial value of the counter ? Enter yes or no : ";
-			std::cin >> val;
-			new_Counter.your_value(val);
-			new_Counter.set_schetchik();
-			bool_value = new_Counter.bool_val(val);
+			std::cout << "Enter the initial value of the counter: ";
+			std::cin >> schet;
+			Counter new_Counter(schet, val);
+			new_Counter.set_equals_schetchik();
 		}
+		else if (val == "no")
+		{
+			schet = 0;
+			val = "";
+			Counter new_Counter(schet, val);
+			new_Counter.set_equals_schetchik();
+		}
+		else
+		{
+			std::cout << "Error! repeated question..." << std::endl;
+			goto loop;
+		}
+
+		Counter new_Counter(schet, val);
+
 		do {
 			std::cout << "Enter the command ('+', '-', '=' or 'x'): ";
 			std::cin >> val;
-			new_Counter.effect(val);
+
+			if (val == "+")
+			{
+				new_Counter.set_plus_schetchik();
+			}
+			else if (val == "-")
+			{
+				new_Counter.set_minus_schetchik();
+			}
+			else if (val == "=")
+			{
+				new_Counter.set_equals_schetchik();
+			}
+			else if (val == "x")
+			{
+				std::cout << "Goodbuy!" << std::endl;
+			}
 		} while (val != "x");
-		if (val == "x") { break; }
+
 	} while (true);
 
 	return 0;
